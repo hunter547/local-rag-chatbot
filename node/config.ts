d3d84060803e7e6cb3357cbd5d62cb81d91c07fe
Hunter Evanoff <hunter547@gmail.com>
2024-01-24 18:20:35 -0700

    Integrate gpt4all and use backroad to construct an llm chat ui.

--- node/config.ts
+++ node/config.ts
@@ -5,19 +5,30 @@ import { ChromaLibArgs } from "langchain/dist/vectorstores/chroma";
 import { loadModel } from "gpt4all";
 import dotenv from "dotenv";
 dotenv.config({
-  path: "../.env",
+	path: "../.env",
 });
 
-export const GPT4ALL_LLM = async () =>
-  await loadModel("gpt4all-13b-snoozy-q4_0.gguf", {
-    verbose: true,
-    allowDownload: false,
-    modelPath: "/Users/h.evanoff/.cache/gpt4all/",
-  });
+export const GPT4ALL_LLM = async () => {
+	const PATH = process.env.GPT4ALL_PATH;
+	const MODEL = process.env.GPT4ALL_MODEL;
+	const MODEL_LIST = process.env.GPT4ALL_MODEL_LIST;
+	if (!PATH || !MODEL || !MODEL_LIST) {
+		throw new Error(
+			"PATH, MODEL, and MODEL_LIST need to be set in the enviroment variables to locate the GPT4ALL model in your filesystem, as well as confirming the model passed in is contained in the available model list."
+		);
+	}
+	return await loadModel(MODEL, {
+		verbose: true,
+		allowDownload: false,
+		modelPath: PATH,
+		modelConfigFile: MODEL_LIST,
+		device: "gpu",
+	});
+};
 
 export const HF_EMBEDDINGS = new HuggingFaceInferenceEmbeddings();
 
 export const CHROMA_DB_CONFIG: ChromaLibArgs = {
-  url: "http://localhost:8000",
-  collectionName: "primeautocare",
+	url: "http://localhost:8000",
+	collectionName: "primeautocare",
 };
--- node/retrieve.ts
+++ node/retrieve.ts
@@ -5,65 +5,144 @@ import { CHROMA_DB_CONFIG, GPT4ALL_LLM, HF_EMBEDDINGS } from "./config";
 import { ScoreThresholdRetriever } from "langchain/retrievers/score_threshold";
 import { ParentDocumentRetriever } from "langchain/retrievers/parent_document";
 import { createDocumentStoreFromByteStore } from "langchain/storage/encoder_backed";
-import { RetrievalQAChain } from "langchain/chains";
-import { PromptTemplate } from "@langchain/core/prompts";
-import { BaseLanguageModelInterface } from "@langchain/core/language_models/base";
+import { createCompletion } from "gpt4all";
+import { run } from "@backroad/backroad";
+
+const LLM = GPT4ALL_LLM();
+const LOADING_STRING = "Searching...";
+
+async function getResponse(
+	query: string,
+	parentDocumentRetriever: ParentDocumentRetriever
+) {
+	const relevantDocuments = await parentDocumentRetriever.getRelevantDocuments(
+		query
+	);
+
+	const context = relevantDocuments.map((doc) => doc.pageContent).join("\n");
+
+	const template = `[INST] <<SYS>> Use the following pieces of context to answer the question at the end. \
+		  If you don't know the answer, just say that you don't know, don't try to make up an answer. \
+		  Use three sentences maximum and keep the answer as concise as possible. Don't mention context in the response. <</SYS>> \
+		  ${context} \
+		  Question: ${query} \
+		  Helpful Answer:[/INST]`;
+
+	console.log(
+		"Initiating chat completion for query:",
+		`"${query.replaceAll("\n", "")}"\nThis may take some time...`
+	);
+	const response = await createCompletion(await LLM, [
+		{
+			role: "user",
+			content: template,
+		},
+	]);
+
+	return {
+		llmResponse: response.choices[0].message.content,
+		relevantDocuments,
+	};
+}
 
 async function main() {
-  // Docstore and retreiver setup
-  const docstore = createDocumentStoreFromByteStore(
-    await LocalFileStore.fromPath("./full_docs")
-  );
-  const parentSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000,
-    chunkOverlap: 0,
-  });
+	// Docstore and retreiver setup
+	const docstore = createDocumentStoreFromByteStore(
+		await LocalFileStore.fromPath("./full_docs")
+	);
+	const parentSplitter = new RecursiveCharacterTextSplitter({
+		chunkSize: 1000,
+		chunkOverlap: 0,
+	});
 
-  const childSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 400,
-    chunkOverlap: 0,
-  });
+	const childSplitter = new RecursiveCharacterTextSplitter({
+		chunkSize: 400,
+		chunkOverlap: 0,
+	});
 
-  const chromaStore = new Chroma(HF_EMBEDDINGS, CHROMA_DB_CONFIG);
-  await chromaStore.ensureCollection();
+	const chromaStore = new Chroma(HF_EMBEDDINGS, CHROMA_DB_CONFIG);
+	await chromaStore.ensureCollection();
 
-  const childDocumentRetriever = ScoreThresholdRetriever.fromVectorStore(
-    chromaStore,
-    { minSimilarityScore: 0.01, maxK: 3 }
-  );
+	const childDocumentRetriever = ScoreThresholdRetriever.fromVectorStore(
+		chromaStore,
+		{ minSimilarityScore: 0.01, maxK: 3 }
+	);
 
-  const parentDocumentRetriever = new ParentDocumentRetriever({
-    vectorstore: chromaStore,
-    childSplitter,
-    parentSplitter,
-    childDocumentRetriever,
-    docstore,
-  });
+	const parentDocumentRetriever = new ParentDocumentRetriever({
+		vectorstore: chromaStore,
+		childSplitter,
+		parentSplitter,
+		childDocumentRetriever,
+		docstore,
+	});
 
-  const template =
-    "[INST] <<SYS>> Use the following pieces of context to answer the question at the end. \
-    If you don't know the answer, just say that you don't know, don't try to make up an answer. \
-    Use three sentences maximum and keep the answer as concise as possible. <</SYS>> \
-    {context} \
-    Question: {question} \
-    Helpful Answer:[/INST]";
+	run(async (br) => {
+		br.title({ label: "Pride Auto Care Chat 🤖" });
+		br.write({
+			body: `# Pride Auto Care Chat 🤖`,
+		});
+		const messages = br.getOrDefault("messages", [
+			{ by: "ai", content: "What would you like to know?" },
+		]);
 
-  const chain = RetrievalQAChain.fromLLM(
-    (await GPT4ALL_LLM()) as unknown as BaseLanguageModelInterface,
-    parentDocumentRetriever,
-    {
-      prompt: new PromptTemplate({
-        inputVariables: ["context", "question"],
-        template,
-      }),
-      returnSourceDocuments: true,
-    }
-  );
+		messages.forEach((message) => {
+			br.chatMessage({ name: message.by }).write({ body: message.content });
+		});
 
-  const results = await chain.invoke({
-    query: "Are there any tire services provided?",
-  });
-  console.log(results);
+		const input = br.chatInput({ id: "input" });
+		const alreadySearching = messages
+			.map((message) => message.content)
+			.includes(LOADING_STRING);
+		if (input && !alreadySearching) {
+			br.setValue("messages", [
+				...messages,
+				{ by: "human", content: input },
+				{ by: "ai", content: LOADING_STRING },
+			]);
+		} else if (!input && alreadySearching) {
+			const currentInput =
+				messages[
+					messages.findIndex((message) => message.content === LOADING_STRING) -
+						1
+				].content;
+			if (currentInput) {
+				const response = await getResponse(
+					currentInput,
+					parentDocumentRetriever
+				);
+				const filteredMessages = messages.filter(
+					(message) => message.content !== LOADING_STRING
+				);
+				br.setValue("messages", [
+					...filteredMessages,
+					{
+						by: "ai",
+						content: `${response.llmResponse}
+
+### Source Documents
+
+---
+
+${response.relevantDocuments
+	.map((doc, i) => {
+		let text = `
+
+**Doc ${i + 1}**
+
+> ${doc.pageContent.replace(/(\n){2,}/g, "\n")}
+
+URL: [${doc.metadata.url}](${doc.metadata.url})
+
+---`;
+		return text;
+	})
+	.join("\n")}
+`,
+					},
+				]);
+			}
+		}
+	});
 }
 
 main();
